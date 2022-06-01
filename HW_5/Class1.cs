caf6c74694dae3f24fef17a14352acd3225d116c
SergeyBakunin <serge.bakunin@gmail.com>
2022-06-01 16:53:31 +0300

    Добавьте файлы проекта.

---
+++ HW_5/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5
+{
+    public class Sample
+    {
+        private int param;
+
+        public int Param
+        {
+            get { return param; }
+            set { param = value; }
+        }
+
+        public void Run(int a)
+        {
+            Console.WriteLine($"Run {a} ...");
+        }
+    }
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Sample sample1 = new Sample();
+            sample1.Param = 10;
+
+            Sample sample2 = new Sample();
+            sample2.Param = 20;
+
+            Type type1 = typeof(Sample);
+            Type type2 = sample1.GetType();
+
+            PropertyInfo propertyInfo = type1.GetProperty("Param");
+
+            if (propertyInfo.CanRead)
+            {
+                int paramValue = (int)propertyInfo.GetValue(sample2);
+                Console.WriteLine($"value -> {paramValue}");
+            }
+
+            if (propertyInfo.CanWrite)
+            {
+                propertyInfo.SetValue(sample2,50);
+            }
+
+            if (propertyInfo.CanRead)
+            {
+                int paramValue = (int)propertyInfo.GetValue(sample2);
+                Console.WriteLine($"value -> {paramValue}");
+            }
+
+
+            MethodInfo methodInfo = 
+                type2.GetMethods().FirstOrDefault(method => method.Name == "Run");
+            methodInfo.Invoke(sample1, new object[] { 100 });
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ HW_5/HW_5.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5
+{
+    internal class HW_5
+    {
+        static void PrintDir(DirectoryInfo dir, string indent, bool lastDirectory)
+        {
+            Console.Write(indent);
+            Console.Write(lastDirectory ? "└───" : "├──");
+            indent += lastDirectory ? "  " : "│";
+            Console.WriteLine(dir.Name);
+
+            //TODO: Распечатать наименования всех файлов
+            
+
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                PrintDir(subDirs[i], indent, i == subDirs.Length - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Exercise_1
+            Console.WriteLine("Exercise_1");
+            Console.WriteLine("_______________________");
+           // string fileName_1 = "text.txt";
+            Console.WriteLine("Введите сообщение: ");
+            string str = Console.ReadLine();
+            File.WriteAllText("text.txt", str);
+            //string fileText = File.ReadAllText(fileName_1);
+            Console.WriteLine("Ваше сообщение в файле: " + File.ReadAllText("text.txt"));
+            Console.WriteLine();
+
+            // Exercise_2
+            Console.WriteLine("Exercise_2");
+            Console.WriteLine("_______________________");
+           // string fileName_2 = "startup.txt";
+            string currentTime = DateTime.Now.ToString();
+            File.WriteAllText("startup.txt", currentTime);
+            //string fileText_2 = File.ReadAllText(fileName_2);
+            Console.WriteLine("Cообщение в файле: " + File.ReadAllText("startup.txt"));
+            Console.WriteLine();
+
+            // Exercise_3
+            Console.WriteLine("Exercise_3");
+            Console.WriteLine("_______________________");
+            Console.WriteLine("Ввести с клавиатуры произвольный набор чисел(0...255): ");     
+            string numbStr = Console.ReadLine();
+            string[] numbStrArr = numbStr.Split();
+            byte[] numbArr = new byte[100];
+            for (int i = 0; i < numbStrArr.Length; i++)
+            {
+                Convert.ToByte(numbStrArr[i]);
+                numbArr[i] = Convert.ToByte(numbStrArr[i]);
+            };
+            File.WriteAllBytes("bytes.bin", numbArr);
+            Console.WriteLine("Файл записан.");
+            Console.WriteLine();
+
+            // Exercise_4
+            Console.WriteLine("Exercise_4");
+            Console.WriteLine("_______________________");
+            PrintDir(new DirectoryInfo(@"C:\Users\Serge\YandexDisk-serge.bakunin1997@yandex.ru\GB\Lessons\HW_5"),"", true);
+            
+            // Не понимаю, как добавить файлы в иерархию и сохранить дерево в файл
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
